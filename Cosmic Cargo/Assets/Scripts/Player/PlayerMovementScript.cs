3a64900e1e1d62320ad6cff548857a6bfddad0fe
CallumMyers4 <callummyers4@gmail.com>
2025-02-07 16:45:46 +0000

    Added dash mechanic
    Player can no longer move whilst in station view

--- Cosmic Cargo/Assets/Scripts/Player/PlayerMovementScript.cs
+++ Cosmic Cargo/Assets/Scripts/Player/PlayerMovementScript.cs
@@ -5,18 +5,24 @@ using UnityEngine;
 
 public class PlayerMovementScript : MonoBehaviour
 {
-    private float moveSpeed = 8f;   //players move speed
+    private float moveSpeed = 8.0f;   //players move speed
     private Rigidbody2D rb;  //rigidbody ref
     public Camera playerCam, stationCam;  //reference to player's cam and station's cam
 
     private Vector2 movement;   //store inputs
     private Vector2 mousePos;   //mouse pos in world space
+    
+    //check if currently dashing, how long dash should last, how long is left before timer runs out, when dash was last used and
+    //how quick to move whilst dashing and how long between allowing dash
+    private bool isDashing; private float dashTime = 5.0f, dashTimeLeft = 0.0f, lastDashTime = 0.0f, dashSpeed = 12.0f, dashCooldown = 10.0f;
+
 
     void Start()
     {
         //fill rb ref
         rb = GetComponent<Rigidbody2D>();
     }
+
     void Update()
     {
         //get input
@@ -37,16 +43,58 @@ public class PlayerMovementScript : MonoBehaviour
             stationCam.enabled = false;
             playerCam.enabled = true;
         }
+
+        //check if player can/wants to dash
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time >= lastDashTime + dashCooldown)
+        {
+            StartDash();
+        }
     }
 
     void FixedUpdate()
     {
-        //move player
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        //move player depending on whether or not dash is currently enabled
+        if (isDashing && playerCam.enabled)
+        {
+            Dash();
+        }
+        else if (playerCam.enabled)
+        {
+            Move();
+        }
         
-        //get direction towards mouse
+        //look towards mouse pos
         Vector2 lookDir = mousePos - rb.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
         rb.rotation = angle;
+    }
+
+    //move at normal speed
+    void Move()
+    {
+        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    //begin dashing
+    void StartDash()
+    {
+        isDashing = true;
+        dashTimeLeft = dashTime;
+        lastDashTime = Time.time;
+    }
+
+    //move at dash speed
+    void Dash()
+    {
+        if (dashTimeLeft > 0)
+        {
+            rb.velocity = movement.normalized * dashSpeed;
+            dashTimeLeft -= Time.fixedDeltaTime;
+        }
+        else
+        {
+            isDashing = false;
+            rb.velocity = Vector2.zero; // Stop dash
+        }
     }
 }
