2910395f7cf4b9956f5455e9a1df9002333c7bab
CallumMyers4 <callummyers4@gmail.com>
2025-02-09 16:34:42 +0000

    Enemies spawn
    Initial wait is 5s
    Stage 2 wait is 2.5s

--- Cosmic Cargo/Assets/Scripts/Managers/GameManagerScript.cs
+++ Cosmic Cargo/Assets/Scripts/Managers/GameManagerScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Microsoft.Unity.VisualStudio.Editor;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Animations;
 using UnityEngine.SceneManagement;
@@ -12,13 +13,15 @@ public class GameManagerScript : MonoBehaviour
     public GameObject leftBorder, rightBorder, bottomBorder, topBorder;    //borders of game world
     private float minX, maxX, minY, maxY;   //boundaries of spawning
     public GameObject partsPrefab, shipAttackerPrefab, playerAttackerPrefab; //prefabs for the collectables and enemies
-    private float spawnInterval = 2f; // time in seconds between enemy spawns
+    private float spawnInterval = 5f; // time in seconds between enemy spawns
     [SerializeField]
     private PlayerMovementScript player;  //player ref
     [SerializeField]
     public UnityEngine.UI.Image healthBar, enemyBar, dashBar, partsBar; //ref to healthbar, bar displaying enemies in danger zone, dash cooldown bar and parts found
     [SerializeField]
     private GameObject HUDPanel, pausePanel; //ref to main HUD and pause menu
+    public int stage = 1;
+    private bool spawningEnemies = true; //enemy spawning
 
     // Start is called before the first frame update
     void Start()
@@ -38,6 +41,9 @@ public class GameManagerScript : MonoBehaviour
 
             Instantiate(partsPrefab, collectableSpawn, Quaternion.identity);
         }
+
+        //start spawning enemies
+        StartCoroutine(SpawnEnemy());
     }
 
     // Update is called once per frame
@@ -64,6 +70,25 @@ public class GameManagerScript : MonoBehaviour
                 ClosePause();
             }
         }
+
+        if (player.partsCollected == 10 && stage == 1)
+        {
+            //spawn next round collectables
+            for (int i = 0; i < partsPerRound; i++)
+            {
+                float randomX = Random.Range(minX, maxX);
+                float randomY = Random.Range(minY, maxY);
+                Vector2 collectableSpawn = new Vector2(randomX, randomY);
+                Instantiate(partsPrefab, collectableSpawn, Quaternion.identity);
+
+                player.partsCollected = 0;
+                spawnInterval = 2.5f;
+            }
+
+            stage = 2;
+        }
+        else if (player.partsCollected == 10 && stage == 2)
+            Win();
     }
 
     //opens pause, closes HUD
@@ -83,5 +108,38 @@ public class GameManagerScript : MonoBehaviour
     public void LoadMenu()
     {
         SceneManager.LoadScene("MainMenuScene");
+    }
+
+    public void Win()
+    {
+        Debug.Log("Won!");
+    }
+
+    //spawn enemies
+    private IEnumerator SpawnEnemy()
+    {
+        while (spawningEnemies) // Keep spawning enemies until the game ends
+        {
+            yield return new WaitForSeconds(spawnInterval);
+
+            float randomX = Random.Range(minX, maxX);
+            float randomY = Random.Range(minY, maxY);
+            Vector2 enemySpawn = new Vector2(randomX, randomY);
+            
+            //randomly select which enemy to spawn
+            GameObject enemyPrefab;
+            float randomValue = Random.value;
+
+            if (randomValue < 0.4f) // 40% chance
+            {
+                enemyPrefab = shipAttackerPrefab;
+            }
+            else // 60% chance
+            {
+                enemyPrefab = playerAttackerPrefab;
+            }
+
+            Instantiate(enemyPrefab, enemySpawn, Quaternion.identity);
+        }
     }
 }
